029d3a8bd20c08bb11af17db47739fe2dc2fb87a
Daniela Cano Garcia <159411509+DanielaCanoGarcia@users.noreply.github.com>
2025-06-15 19:28:32 -0600

    model tournaments

--- src/modules/tournaments/tournaments.module.ts
+++ src/modules/tournaments/tournaments.module.ts
@@ -1,15 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TournamentController } from './controllers/tournament.controller';
+import { FindAllTournamentsUseCase } from './usecase/find-all-tournaments.usecase';
 import { CreateTournamentUseCase } from './usecase/create-tournament.usecase';
+import { DeleteTournamentUseCase } from './usecase/delete-tournament.usecase';
 import { TournamentRepository } from './infrastructure/tournament.repository';
 import { PrismaService } from '../../database/prisma/prisma.service';
+import { UpdateTournamentUseCase } from './usecase/update-tournament.usecase';
 
 @Module({
   controllers: [TournamentController],
   providers: [
+    FindAllTournamentsUseCase,
     CreateTournamentUseCase,
     TournamentRepository,
     PrismaService,
+    FindAllTournamentsUseCase,
+    UpdateTournamentUseCase,
+    DeleteTournamentUseCase,
   ],
 })
 export class TournamentsModule {}
